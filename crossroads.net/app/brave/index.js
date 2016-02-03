806f88556e38276911bc78e3b1ff8f8a2ee928ae
Taylor C. MacDonald <tcmacdonald@gmail.com>
2016-02-03 15:16:05 -0500

    Implement multiple question types.

--- crossroads.net/app/brave/index.js
+++ crossroads.net/app/brave/index.js
@@ -7,7 +7,6 @@
   require('./templates/welcome.html');
   require('./templates/login.html');
   require('./templates/join.html');
-  require('./templates/question.html');
   require('./templates/host.html');
   require('./templates/dashboard.html');
 
--- crossroads.net/app/brave/question.directive.js
+++ crossroads.net/app/brave/question.directive.js
@@ -3,19 +3,28 @@
 
   module.exports = QuestionDirective;
 
+  require('./templates/input_radio.html');
+  require('./templates/input_text.html');
+  require('./templates/input_checkbox.html');
+  require('./templates/input_select.html');
+
   QuestionDirective.$inject = ['$log'];
 
   function QuestionDirective($log) {
     return {
-      restrict: 'EA',
+      restrict: 'AE',
       scope: {
         key: '@key',
-        answers: '=',
-        label: '=',
+        definition: '=',
         responses: '=responses',
       },
       transclude: true,
-      templateUrl: 'templates/question.html'
+      template: '<ng-include src="getTemplateUrl()" />',
+      controller: ['$scope', function($scope){
+        $scope.getTemplateUrl = function () {
+          return 'templates/input_'+ $scope.definition.input_type +'.html';
+        };
+      }]
     };
   }
 
